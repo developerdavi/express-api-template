37b911a46806f90e94920e9a3228a73c599b76c2
Davi Rodrigues <developer.davi@gmail.com>
2020-03-03 15:21:10 -0300

    Added routes

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ const http = require('http')
 const cors = require('cors')
 
 const settings = require('./Settings')
+const routes = require('./Routes')
 
 const app = express()
 const server = http.Server(app)
 
 app.use(cors())
+
+app.use(routes.public)
+app.use(routes.private)
 
 server.listen(settings.PORT, () => {
   console.log(`[ SERVER INFO ] RUNNING ON PORT ${settings.PORT}`)
